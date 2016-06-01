e9f2b12d6a39a0c999c08825323c041cd441e241
jkuelz <jmkuelz@uw.edu>
2016-06-01 00:20:33 -0700

    test

--- cmd/lab7/main.go
+++ cmd/lab7/main.go
@@ -51,14 +51,14 @@ func main() {
 			// our site can't handle http status codes, but I'll still put them in cause why not
 			c.JSON(http.StatusOK, gin.H{"error": "true", "message": "db was not created. Check your DATABASE_URL"})
 		} else {
-			c.JSON(http.StatusOK, gin.H{"error": "false", "message": "db createdssssss"})
+			c.JSON(http.StatusOK, gin.H{"error": "false", "message": "db created"})
 		}
 	})
 
 	router.GET("/query1", func(c *gin.Context) {
 		table := "<table class='table'><thead><tr>"
 		// put your query here
-		rows, err := db.Query("SELECT * FROM table1") // <--- EDIT THIS LINE
+		rows, err := db.Query("SELECT * FROM usert") // <--- EDIT THIS LINE
 		if err != nil {
 			// careful about returning errors to the user!
 			c.AbortWithError(http.StatusInternalServerError, err)
@@ -75,13 +75,12 @@ func main() {
 		table += "</thead><tbody>"
 		// declare all your RETURNED columns here
 		var id int      // <--- EDIT THESE LINES
-		var name string //<--- ^^^^
 		for rows.Next() {
 			// assign each of them, in order, to the parameters of rows.Scan.
 			// preface each variable with &
-			rows.Scan(&id, &name) // <--- EDIT THIS LINE
+			rows.Scan(&id) // <--- EDIT THIS LINE
 			// can't combine ints and strings in Go. Use strconv.Itoa(int) instead
-			table += "<tr><td>" + strconv.Itoa(id) + "</td><td>" + name + "</td></tr>" // <--- EDIT THIS LINE
+			table += "<tr><td>" + strconv.Itoa(id) + "</td><td></td></tr>" // <--- EDIT THIS LINE
 		}
 		// finally, close out the body and table
 		table += "</tbody></table>"
