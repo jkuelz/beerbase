8dc8551657ac1fafb51c2955b5549baff75a206c
jkuelz <jmkuelz@uw.edu>
2016-06-01 11:18:09 -0700

    Add files via upload

---
+++ data/ReviewerAcc.sql
@@ -0,0 +1,49 @@
+﻿create table ReviewerAcc (
+	id INT,
+	Username VARCHAR(50),
+	Password VARCHAR(50),
+	userid INT NOT NULL,
+	constraint fk_user_ReviewerAcc
+		foreign key (userid)
+		REFERENCES usert (id)
+);
+insert into ReviewerAcc (id, Username, Password, userid) values (1, 'jgilbert0', 'Syh0Rhzu', 1);
+insert into ReviewerAcc (id, Username, Password, userid) values (2, 'rhoward1', 'ORwQZ9KRpt', 2);
+insert into ReviewerAcc (id, Username, Password, userid) values (3, 'sgrant2', 'ysvTlYmfO', 3);
+insert into ReviewerAcc (id, Username, Password, userid) values (4, 'lhunter3', 'blzPy539U', 4);
+insert into ReviewerAcc (id, Username, Password, userid) values (5, 'plittle4', 'PYbBZ1429k1', 5);
+insert into ReviewerAcc (id, Username, Password, userid) values (6, 'rhawkins5', 'J9zFlz91', 6);
+insert into ReviewerAcc (id, Username, Password, userid) values (7, 'joliver6', 'J7PFauJowu', 7);
+insert into ReviewerAcc (id, Username, Password, userid) values (8, 'adixon7', 'MyDEKAoEkH', 8);
+insert into ReviewerAcc (id, Username, Password, userid) values (9, 'jcastillo8', 'Nm9lA2bPD', 9);
+insert into ReviewerAcc (id, Username, Password, userid) values (10, 'sramos9', 'onu1vb', 10);
+insert into ReviewerAcc (id, Username, Password, userid) values (11, 'krichardsa', '6trw7ed', 11);
+insert into ReviewerAcc (id, Username, Password, userid) values (12, 'mboydb', '9JEU4CMcGfy', 12);
+insert into ReviewerAcc (id, Username, Password, userid) values (13, 'hjohnsonc', 'L0CkfWN', 13);
+insert into ReviewerAcc (id, Username, Password, userid) values (14, 'cdixond', 'E5rElnqCOqaq', 14);
+insert into ReviewerAcc (id, Username, Password, userid) values (15, 'msmithe', 'SMFbIS', 15);
+insert into ReviewerAcc (id, Username, Password, userid) values (16, 'ddiazf', 'Rw9DgtbJuA', 16);
+insert into ReviewerAcc (id, Username, Password, userid) values (17, 'cthomasg', 'U3SFYH', 17);
+insert into ReviewerAcc (id, Username, Password, userid) values (18, 'mhawkinsh', 'EQDlEMBx2b', 18);
+insert into ReviewerAcc (id, Username, Password, userid) values (19, 'jgilberti', 'sVBrW8', 19);
+insert into ReviewerAcc (id, Username, Password, userid) values (20, 'jburnsj', 'EHbejE', 20);
+insert into ReviewerAcc (id, Username, Password, userid) values (21, 'jmartink', 'wXwCvP', 21);
+insert into ReviewerAcc (id, Username, Password, userid) values (22, 'rcollinsl', 'BlLNoZWPe', 22);
+insert into ReviewerAcc (id, Username, Password, userid) values (23, 'abryantm', 'KdFqbgv', 23);
+insert into ReviewerAcc (id, Username, Password, userid) values (24, 'mbelln', 'yhIW7t', 24);
+insert into ReviewerAcc (id, Username, Password, userid) values (25, 'lgibsono', 'X72jb5e', 25);
+insert into ReviewerAcc (id, Username, Password, userid) values (26, 'estephensp', 'aNVB36ShH', 26);
+insert into ReviewerAcc (id, Username, Password, userid) values (27, 'jschmidtq', 'OO5shCchd', 27);
+insert into ReviewerAcc (id, Username, Password, userid) values (28, 'dharrisr', 'tjklJDX', 28);
+insert into ReviewerAcc (id, Username, Password, userid) values (29, 'cperkinss', 'phMem4REr', 29);
+insert into ReviewerAcc (id, Username, Password, userid) values (30, 'thickst', 'lrnHLs', 30);
+insert into ReviewerAcc (id, Username, Password, userid) values (31, 'bhendersonu', 'DireFjYpyt', 31);
+insert into ReviewerAcc (id, Username, Password, userid) values (32, 'jstewartv', '8ARV6EXkVI', 32);
+insert into ReviewerAcc (id, Username, Password, userid) values (33, 'rfloresw', 'Bs1iL2Yfc', 33);
+insert into ReviewerAcc (id, Username, Password, userid) values (34, 'bbaileyx', 'LupUTR1', 34);
+insert into ReviewerAcc (id, Username, Password, userid) values (35, 'dpattersony', 't1Cv1YmjG', 35);
+insert into ReviewerAcc (id, Username, Password, userid) values (36, 'pnguyenz', 'IiB8ZgrCLLYH', 36);
+insert into ReviewerAcc (id, Username, Password, userid) values (37, 'pmeyer10', 'CjgGRB', 37);
+insert into ReviewerAcc (id, Username, Password, userid) values (38, 'jmorris11', 'FhKHk2Z', 38);
+insert into ReviewerAcc (id, Username, Password, userid) values (39, 'mkelly12', '4NzpAnf8nT5', 39);
+insert into ReviewerAcc (id, Username, Password, userid) values (40, 'pfields13', 'V2G5AOmkf4', 40);
