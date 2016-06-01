693286c7a81cfa16bbd3ec93a98cda6b0820eaac
jkuelz <jmkuelz@uw.edu>
2016-06-01 00:18:17 -0700

    test

--- cmd/lab7/main.go
+++ cmd/lab7/main.go
@@ -51,7 +51,7 @@ func main() {
 			// our site can't handle http status codes, but I'll still put them in cause why not
 			c.JSON(http.StatusOK, gin.H{"error": "true", "message": "db was not created. Check your DATABASE_URL"})
 		} else {
-			c.JSON(http.StatusOK, gin.H{"error": "false", "message": "db created"})
+			c.JSON(http.StatusOK, gin.H{"error": "false", "message": "db createdssssss"})
 		}
 	})
 
