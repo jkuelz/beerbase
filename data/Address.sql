45e5da6361ea99cd933866596492f6b9a0a99a58
jkuelz <jmkuelz@uw.edu>
2016-06-01 11:53:05 -0700

    Add files via upload

---
+++ data/Address.sql
@@ -0,0 +1,16 @@
+﻿create table Address (
+	id INT PRIMARY KEY,
+	StreetNo VARCHAR(50) NOT NULL,
+	StreetName VARCHAR(50) NOT NULL,
+	City VARCHAR(50) NOT NULL,
+	State VARCHAR(50) NOT NULL,
+	ZipCode INT NOT NULL
+);
+insert into Address (id, StreetNo, StreetName, City, State, ZipCode) values (1, '556', 'Leroy', 'Sacramento', 'CA', 92064);
+insert into Address (id, StreetNo, StreetName, City, State, ZipCode) values (2, '3214', 'Cody', 'Dallas', 'TX', 19300);
+insert into Address (id, StreetNo, StreetName, City, State, ZipCode) values (3, '34', 'Quincy', 'Orlando', 'FL', 47540);
+insert into Address (id, StreetNo, StreetName, City, State, ZipCode) values (4, '983', 'Myrtle', 'San Jose', 'CA', 76301);
+insert into Address (id, StreetNo, StreetName, City, State, ZipCode) values (5, '64025', 'Springview', 'New York City', 'NY', 38971);
+insert into Address (id, StreetNo, StreetName, City, State, ZipCode) values (6, '96', 'Dorton', 'Orlando', 'FL', 22633);
+insert into Address (id, StreetNo, StreetName, City, State, ZipCode) values (7, '419', 'Wayridge', 'Morgantown', 'WV', 38104);
+insert into Address (id, StreetNo, StreetName, City, State, ZipCode) values (8, '1', 'Montana', 'Atlanta', 'GA', 68220);
---
+++ data/Brewery.sql
@@ -0,0 +1,14 @@
+﻿create table Brewery (
+	id INT PRIMARY KEY,
+	addressid INT,
+	Name VARCHAR(50) NOT NULL,
+	FOREIGN KEY (addressid) REFERENCES Address (id)
+);
+insert into Brewery (id, addressid, Name) values (1, 1, 'Régine');
+insert into Brewery (id, addressid, Name) values (2, 2, 'Vénus');
+insert into Brewery (id, addressid, Name) values (3, 3, 'Zhì');
+insert into Brewery (id, addressid, Name) values (4, 4, 'Gaétane');
+insert into Brewery (id, addressid, Name) values (5, 5, 'Simplifiés');
+insert into Brewery (id, addressid, Name) values (6, 6, 'Lài');
+insert into Brewery (id, addressid, Name) values (7, 7, 'Anaël');
+insert into Brewery (id, addressid, Name) values (8, 8, 'Réjane');
