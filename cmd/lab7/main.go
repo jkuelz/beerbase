a5e455afebb0ad97a6097377845ea8f8c1a134bc
Julian Boss <Julian Boss>
2016-06-01 09:42:52 -0700

    test

--- cmd/lab7/main.go
+++ cmd/lab7/main.go
@@ -60,15 +60,35 @@ func main() {
 		user := q["user"]
 		pass := q["pass"]
 
-		if db.Query("SELECT firstName FROM usert WHERE firstName ="+ user +"AND lastName ="+ pass) != nil {
-			c.Data(http.StatusOK, "text/html", []byte("Welcome back"))
+		rows, err := db.Query("SELECT * FROM usert WHERE WHERE firstName ="+ user +"AND lastName ="+ pass) // <--- EDIT THIS LINE
+		if err != nil {
+			// careful about returning errors to the user!
+			c.AbortWithError(http.StatusInternalServerError, err)
+		}
+		// foreach loop over rows.Columns, using value
+		cols, _ := rows.Columns()
+		if len(cols) == 0 {
+			c.AbortWithStatus(http.StatusNoContent)
 		}
 
-		if user != nil {
-			// our site can't handle http status codes, but I'll still put them in cause why not
-			c.JSON(http.StatusOK, gin.H{"error": "true", "message": "db was not created. Check your DATABASE_URL"})
-		} else {
-			c.JSON(http.StatusOK, gin.H{"error": "true", "message": "db was created."})
+		var id int      // <--- EDIT THESE LINES
+		var firstName string
+		var lastName string
+		var birthdate string
+		var gender string
+		var biography string
+		var email string
+
+		for rows.Next() {
+			// assign each of them, in order, to the parameters of rows.Scan.
+			// preface each variable with &
+			rows.Scan(&id,&firstName,&lastName,&birthdate,&gender,&biography,&email) // <--- EDIT THIS LINE
+			// can't combine ints and strings in Go. Use strconv.Itoa(int) instead
+			// table += "<tr><td>" + strconv.Itoa(id) + "</td><td>"+ firstName +"</td><td>"+ lastName +"</td><td>"+ birthdate +"</td><td>"+ gender +"</td><td>"+ biography +"</td><td>"+ email +"</td></tr>" // <--- EDIT THIS LINE
+		}
+
+		if id != nil {
+			c.Data(http.StatusOK, "text/html", []byte("Welcome back"))
 		}
 	})
 
