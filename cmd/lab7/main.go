e1cdce56d8f388c69fc1a8f74d6ec55493a0a853
Julian Boss <Julian Boss>
2016-06-03 08:30:54 -0700

    check

--- cmd/lab7/main.go
+++ cmd/lab7/main.go
@@ -35,17 +35,21 @@ type Review struct {
 	ReviewDate string
 }
 
-// // Example for binding JSON ({"user": "manu", "password": "123"})
-//     router.POST("/loginJSON", func(c *gin.Context) {
-//         var json Login
-//         if c.BindJSON(&json) == nil {
-//             if json.User == "manu" && json.Password == "123" {
-//                 c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
-//             } else {
-//                 c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
-//             }
-//         }
-//     })
+// Example for binding JSON ({"user": "manu", "password": "123"})
+    router.POST("/auth", func(c *gin.Context, req *http.Request) {
+			email := r.URL.Query().Get("email")
+			pass := r.URL.Query().Get("pass")
+			c.JSON(http.StatusUnauthorized, gin.H{"status": email,"pass":pass})
+
+        var json Login
+        if c.BindJSON(&json) == nil {
+            if json.User == "manu" && json.Password == "123" {
+                c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
+            } else {
+                c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+            }
+        }
+    })
 
 func getFeaturedBeer() []Beer {
 	var beerArray []Beer
