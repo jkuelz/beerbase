1763084b07a2239634406a042f67153a3a74cfe3
Julian Boss <Julian Boss>
2016-06-03 09:00:50 -0700

    test

--- cmd/lab7/main.go
+++ cmd/lab7/main.go
@@ -35,52 +35,6 @@ type Review struct {
 	ReviewDate string
 }
 
-// Example for binding JSON ({"user": "manu", "password": "123"})
-    router.POST("/login", func(c *gin.Context)) {
-			email := c.PostForm("email")
-			password := c.PostForm("password")
-
-			if hasIllegalSyntax(email) || hasIllegalSyntax(password) {
-				c.JSON(http.StatusOK, gin.H{"result": "failed", "message": "Don't use syntax that isn't allowed"})
-				return
-			}
-			c.JSON(http.StatusOK, gin.H{"result": "worked", "message": "worked"})
-
-			// // SQL injection in password only
-			// rows, err := db.Query("SELECT ReviewerAcc.email FROM usert.email WHERE usert.email = '" + usernsame + "';")
-			// if err != nil {
-			// 	c.AbortWithError(http.StatusInternalServerError, err)
-			// 	return
-			// }
-			// cols, _ := rows.Columns()
-			// if len(cols) == 0 {
-			// 	c.AbortWithStatus(http.StatusNoContent)
-			// 	return
-			// }
-			//
-			// rowCount := 0
-			// var resultUser string
-			// for rows.Next() {
-			// 	rows.Scan(&resultUser)
-			// 	rowCount++
-			// }
-			// if rowCount > 1 {
-			// 	c.JSON(http.StatusOK, gin.H{"result": "failed", "message": "Too many users returned!"})
-			// 	return
-			// }
-			// // quick way to check if the user logged in properly
-			// if rowCount == 0 {
-			// 	c.JSON(http.StatusOK, gin.H{"result": "failed", "message": "Wrong password/username!"})
-			// 	return
-			// }
-			//
-			// if resultUser == "admin" {
-			// 	c.JSON(http.StatusOK, gin.H{"result": "success", "username": resultUser, "randomCode": rand.Int()})
-			// } else {
-			// 	c.JSON(http.StatusOK, gin.H{"result": "success", "username": resultUser})
-			// }
-    }
-
 func getFeaturedBeer() []Beer {
 	var beerArray []Beer
 	rows, err := db.Query("Select id, name, beerdescription from beer order by random() limit 3")
@@ -184,7 +138,17 @@ func main() {
 	router.Static("/static", "static")
 
 	router.GET("/", indexHandler)
+	// Example for binding JSON ({"user": "manu", "password": "123"})
+	    router.POST("/login", func(c *gin.Context) {
+				email := c.PostForm("email")
+				password := c.PostForm("password")
 
+				if hasIllegalSyntax(email) || hasIllegalSyntax(password) {
+					c.JSON(http.StatusOK, gin.H{"result": "failed", "message": "Don't use syntax that isn't allowed"})
+					return
+				}
+				c.JSON(http.StatusOK, gin.H{"result": "worked", "message": "worked"})
+	    })
 	// router.POST("/addreview", func(c *gin.Context) {
 	// 		user := c.PostForm("ReviewerID")
 	// 		beer := c.PostForm("BeerID")
