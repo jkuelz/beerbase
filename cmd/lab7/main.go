1bf58550e53ea2f11a4d5e6bfc3b03468be5c770
jkuelz <jmkuelz@uw.edu>
2016-06-05 19:37:17 -0700

    queries

--- cmd/lab7/main.go
+++ cmd/lab7/main.go
@@ -238,7 +238,7 @@ func main() {
 			title := c.PostForm("title")
 			reviewDescription := c.PostForm("reviewDescription")
 			log.Println(c.PostForm("rating") + ", "+ c.PostForm("title") + ", "+ c.PostForm("reviewDescription"))
-			_, err := db.Exec("INSERT INTO Review(id, ReviewerID, BeerID, Rating, Title, ReviewDescription, ReviewDate) VALUES((SELECT (MAX(id) + 1) FROM Review), 1, 1, $1, $2, $3, (current_date)", rating, title, reviewDescription)
+			_, err := db.Exec("INSERT INTO Review(id, ReviewerID, BeerID, Rating, Title, ReviewDescription, ReviewDate) VALUES((SELECT (MAX(id) + 1) FROM Review), 1, 1, $1, $2, $3, current_date", rating, title, reviewDescription)
 			if err != nil {
 				log.Println(err)
 				c.AbortWithError(http.StatusInternalServerError, err)
